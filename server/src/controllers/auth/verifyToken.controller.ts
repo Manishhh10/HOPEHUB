3d19f80ba89790bf8de46023b891fb5d775ba3f0
Manishhh10 <manishbhatt2121714@gmail.com>
2025-02-25 16:31:43 +0545

    verifyToken udpated to reutrn currentUser data

--- server/src/controllers/auth/verifyToken.controller.ts
+++ server/src/controllers/auth/verifyToken.controller.ts
@@ -3,12 +3,19 @@ import { Request, Response } from "express";
 import { asyncHandler } from "../../utils/asyncHandler.js";
 import { SuccessResponse } from "../../utils/SuccessResponse.js";
 
-// verifyToken.controller.ts
 export const verifyToken = asyncHandler(async (req: Request, res: Response) => {
-    // Make sure req.user contains the full user data, including id
+    // req.user should now be a plain object with id, first_name, last_name, and email
+    const userData = req.user
+      ? {
+          id: req.user.id,
+          first_name: req.user.first_name,
+          last_name: req.user.last_name,
+          email: req.user.email,
+        }
+      : null;
+
     return res.status(200).json(
-        new SuccessResponse(200, true, "token verified", req.user)
+        new SuccessResponse(200, true, "token verified", userData)
     );
 });
-
 
